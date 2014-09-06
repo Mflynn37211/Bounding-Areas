da4fb5ed9fffc8840a763c6bfe401f5fb4998543
Michael Flynn <Mflynn37211@yahoo.com>
2014-09-06 16:48:19 -0400

    All tests are passing

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,2 +1,20 @@
+# Bounding Area Claa
 class BoundingArea
+  def initialize(box_array)
+    @box_array = box_array
+  end
+
+  def contains_point?(x, y)
+    @box_array.each do |box|
+
+ # working on indentation and keep getting this error from rubucop
+ # lib/bounding_area.rb:10:7: C: Favor modifier if  usage when having a single-line body. #
+ # Another good alternative is the usage of control flow &&/||.
+ # Do you have reccomendations for a best practice in this instance???
+      if box.contains_point?(x, y)
+        return true
+      end
+    end
+      false
+  end
 end
