d55b811c03870a8a6f326a05f709dade7f7ba6a8
Helen Hood <helen.c.hood@gmail.com>
2014-09-09 11:08:25 -0400

    Respond to comment re rubocop message

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -6,15 +6,8 @@ class BoundingArea
 
   def contains_point?(x, y)
     @box_array.each do |box|
-
- # working on indentation and keep getting this error from rubucop
- # lib/bounding_area.rb:10:7: C: Favor modifier if  usage when having a single-line body. #
- # Another good alternative is the usage of control flow &&/||.
- # Do you have reccomendations for a best practice in this instance???
-      if box.contains_point?(x, y)
-        return true
-      end
+      return true if box.contains_point?(x, y)
     end
-      false
+    false
   end
 end
