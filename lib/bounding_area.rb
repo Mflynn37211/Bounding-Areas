a632040db5db197051107d3bd3f64b4000961681
Helen Hood <helen.c.hood@gmail.com>
2014-09-09 11:13:06 -0400

    Add attr_reader for @box_array

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,11 +1,13 @@
 # Bounding Area Claa
 class BoundingArea
+  attr_reader :box_array
+
   def initialize(box_array)
     @box_array = box_array
   end
 
   def contains_point?(x, y)
-    @box_array.each do |box|
+    box_array.each do |box|
       return true if box.contains_point?(x, y)
     end
     false
